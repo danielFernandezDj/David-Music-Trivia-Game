51d5585cb8e85c2e761e5aed61ad7564153f5c0b
David111922 <salve62@yahoo.com>
2024-04-12 19:40:29 -0700

    fixed link issue with index.js

--- assets/index.js
+++ assets/index.js
@@ -9,6 +9,169 @@ const questions = [
       options: ["Elvis Presley", "Michael Jackson", "Prince", "Madonna"],
       answer: "Michael Jackson"
     },
+    {
+      question: "Who was the original guitarist for Led Zeppellin?" ,
+      options: ["Peter Frampton", "Jeff Beck", "Larry Carlton" , "Jimmy Page"],
+      answer: "Jimmy Page"
+    },
+    {
+      question: 'Who wrote the song "I shot the Sherriff"?',
+      options: ["Eric Clapton", "Leon Russell", "Bob Marley", "Steel Pulse"],
+      answer: "Bob Marley"
+    },
+    {
+      question: "Should I Stay or Should I go was a huge hit for which band?",
+      options: ["The Ramones", "Velvet Underground", "The Sex Pistols", "The Clash"],
+      answer: "The Clash"
+    },
+    {
+      question: '"Sweetest Tabboo" and "Smooth Operator" were huge hits for which singer',
+      options: ["Tina Marie", "Whitney Houston", "Sade", "Anita Baker"],
+      answer: "Sade"
+    },
+    {
+      question: 'Who wrote the jazz standard" So What?',
+      options: ["John Coltrane", "Wes Montgomery", "Thelonius Monk", "Miles Davis"],
+      answer: "Miles Davis"
+    },
+    {
+      question: 'Who was dubbed "The Queen of Salsa"?',
+      options: ["Celia Cruz", "Olga Guillot", "Lola Flores", "La Lupe"],
+      answer: "Celia Cruz"
+    },
+    {
+      question: 'The show "Bad Boys" had people singing the theme song . What was the name of this band ?',
+      options: ["UB40", "Inner Circle", "Third World", "Aswad"],
+      answer: "Inner Circle"
+    },
+    {
+      question: '"Birdland" was a huge hit for this jazz Giant in 1971',
+      options: ["Charlie Parker", "Glen Miller", "Tower of Power", "Weather Report"],
+      answer: "Weather Report"
+    },
+    {
+      question: "Who was the singer for the band Thin Lizzy",
+      options: ["Sting", "Phil Lynott", "Steve Perry", "Rick Emmett"],
+      answer: "Phil Lynott"
+    },
+    {
+      question: "Who was known as the 'Old Blue Eyes'?",
+      options: ["Frank Sinatra", "David Coverdale", "Andy Williams", "Perry Como"],
+      answer: "Frank Sinatra"
+    },
+    {
+      question: "Moonglow was a big hit in 1936 for this band leader ?",
+      options: ["Artie Shaw", "Tommy Dorsey", "Dave Brubeck", "Benny Goodman"],
+      answer: "Benny Goodman"
+    },
+    {
+      question: '"Breezin was a huge hit for this great guitrist"',
+      options: ["Eddie Van Halen", "Mateo Mancusso", "Carlos Santana", "George Benson"],
+      answer: "George Benson"
+    },
+    {
+    
+      question: "What year did the Beatles play on'Ed Sullivan' ?",
+      options: ["1960", "1970", "1964", "1972"],
+      answer: "1964"
+    },
+    {
+      question: "Who was the original singer for Genesis?",
+      options: ["David Vedder", "Phil Collins", "Billy Squire", "Peter Gabriel"],
+      answer: "Peter Gabriel"
+    },
+    {
+      question: "Maybe the greatest rock guitar solos ever. Who was the guitarist on 'Eruption'?",
+      options: ["Jimi Hendrix", "Steve Vai", "Joe Satriani", "Eddie Van Halen"],
+      answer: "Eddie Van Halen"
+    },
+    {
+      question: "The song,'Bamboleo' went mainstream for this Rumba Flamenca band'?",
+      options: ["Los Del Rio", "Gypsy Kings", "Gente De Zona", "Habana de Primera"],
+      answer: "Gypsy Kings"
+    },
+    {
+      question: "Arguably the greatest Flamenco guitarist ever.Who wrote and plaed the orinal song 'Entre Dos Aguas'?",
+      options: ["Diego del Morao", "Tomatito", "Paco De Lucia", "Sabicas"],
+      answer: "Paco De Lucia"
+    },
+    {
+      question: "Which of these songs is not by Lynard Skynard'?",
+      options: ["Sweet Home Alabama", "Mississippi Queen", "Tuesday's Gone", "Mr.Saturday Night"],
+      answer: "Mississippi Queen"
+    },
+    {
+      question: "The orangutang in the Jungle Book sings 'I wanna be like you'Who is the vice behind the primate?'",
+      options: ["Dean Martin", "Bing Crosby", "Louie Prima ", "Bobby Darin"],
+      answer: "Louie Prima "
+    },
+    {
+      question: "What year wasthe Pink Floyd album'Dark Side of the Moon' released'?",
+      options: ["1973", "1969", "1978", "1982"],
+      answer: "1973"
+    },
+    {
+      question: "who was Garfunkel's singing partner?",
+      options: ["Garfinkle ", "Patsy Cline", "Simon", "Garfield"],
+      answer: "Simon"
+    },
+    {
+      question: "Geddy Lee was the lead singer which famous rock band?",
+      options: ["King Crimson", "Rush", "Triumph", "Boston"],
+      answer: "Rush"
+    },
+    {
+
+      question: "in this song by Eddie Money. how many tickets does he have to Paradise?",
+      options: ["1", "8", "2", "3"],
+      answer: "2"
+    },
+    {
+      question: "Who is Jane through with in this 1988 hit by Jane's Addiction'?",
+      options: ["Romeo", "Everybody","Sergio", "Cameo's"],
+      answer: "Sergio"
+    },
+    {
+      question: "In 1974 Led Zeppelin decided to launch thei own record label. What was its name?",
+      options: ["Led Balloon", "Cosmic", "Satellite", "Swan Song"],
+      answer: "Swan Song"
+    },
+    {
+      question: "This all female rock band had a big hit with the song 'What's Up'.What is the name of this band?",
+      options: ["The Go-Go's", "The Vandals", "the Bangles", "4 Non Blondes"],
+      answer: "4 Non Blondes"
+    },
+    {
+      question: "Which band released 'Hard to Handle' in 1990?",
+      options: ["Nirvana", "Rage Against the Machine", "Black Crowes", "Molly Hatchet"],
+      answer: "Black Crowes"
+    },
+    {
+      question: "Who is the lead singer for Pearl Jam'?",
+      options: ["Perry Comoy", "Eddie Vedder", "Dave Grohl", "Michael Stipe"],
+      answer: "Eddie Vedder"
+    },
+    {
+      question: "Who was the drummer for the band , The Police?",
+      options: ["Dave Grohl", "Stewart Copeland", "John Bonham", "Neil Pert"],
+      answer: "Stewart Copeland"
+    },
+    {
+      question: "'Reasons' was a huge hit for this band in the early 70's'?",
+      options: ["Parliament", "Kool and the Gang", "Earth Wind and Fire", "The O'jays"],
+      answer: "Earth Wind and Fire"
+    },
+
+
+
+
+
+
+
+
+
+
+    
 
     
     // Add more questions here
